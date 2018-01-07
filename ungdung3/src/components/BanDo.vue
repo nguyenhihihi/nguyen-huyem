b7c98e1afd574247c00b54e593355e9f6e2df044
huyenphuong <dtphuyen2506@gmail.com>
2018-01-07 10:23:32 +0700

    Fix bug null object Hien thi duong di tu xe toi khach

--- ungdung3/src/components/BanDo.vue
+++ ungdung3/src/components/BanDo.vue
@@ -13,6 +13,8 @@
 
 	import {DsDaCoXeNhan} from '../firebase'; 
 	import {Driver} from '../firebase'; 
+	import {req} from '../firebase'; 
+	import {db} from '../firebase'; 
 export default{
 	name: "BanDo",
 	data(){
@@ -26,14 +28,16 @@ export default{
 	{
 		dsDaCoXeNhan: DsDaCoXeNhan,
 		driver: Driver,
+		r: req,
 	},
 	mounted (){
 
 		var key = this.$route.params.key;
 		this.luuid(key);
 		//this.dieuChinhDuLieu();
-		this.timKhach();
+		//this.timKhach();
 		//this.timXe();
+		this.thu();
 
 		
 	},
@@ -48,6 +52,62 @@ export default{
 
 
 	methods: {
+		thu(){	
+			var datxe;
+			var taixe;
+
+			var self = this;
+			var kq = db.ref('reqDatXe/'+ self.key);
+			var keyXeRuoc;
+			kq.on('value', function(snapshot){
+				datxe = snapshot.val();
+				 keyXeRuoc = snapshot.val().xeRuoc;
+				 var kq2 = db.ref('driver/'+ keyXeRuoc);
+				kq2.on('value', function(snapshot2){
+				taixe = snapshot2.val();
+				console.log(datxe);
+				console.log(taixe);
+				self.initMap(taixe.lat, taixe.lng, datxe.lat, datxe.long)
+
+				//init map
+
+			});
+			});
+		},
+		initMap(posXeLat, posXeLong, posKhachLat, posKhachLong) {
+          var posXe = {lat: posXeLat , lng: posXeLong};
+          var posKhach = {lat: posKhachLat, lng: posKhachLong};
+           if (typeof posXe.lat === 'undefined'|| typeof posXe.lng === 'undefined' || typeof posKhach.lat === 'undefined' || typeof posKhach.lng === 'undefined')
+           {   
+            var loi = "</i></b><h4>Đang tải dữ liệu...</h4></b></i>";
+            $('#map').empty();
+            $('#map').append(loi);
+
+            return;
+           }
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: posXe
+        });
+         var directionDisplay = new google.maps.DirectionsRenderer({
+          map:map
+         });
+
+         var request = {
+          destination: posXe,
+          origin: posKhach,
+          travelMode:'DRIVING'
+         };
+
+         var directionService = new google.maps.DirectionsService();
+
+        directionService.route(request, function(response, status){
+          if (status == 'OK'){
+            directionDisplay.setDirections(response);
+          }
+        });
+      },
 	dieuChinhDuLieu()
 		{
 			// var self = this;
@@ -70,6 +130,7 @@ export default{
 			var i = 0;
 			var n = self.dsDaCoXeNhan.length;
 			console.log(self.dsDaCoXeNhan);
+			console.log("Mong cho: ");
 			console.log(Object.values(self.dsDaCoXeNhan));
 			//console.log(self.dsDaCoXeNhan["Observer"]);
 			console.log("so luong diem: ");
@@ -159,3 +220,12 @@ export default{
 	}
 }	
 </script>
+  <style type="text/css">
+     #map {
+        height: 550px;
+        width: 100%;
+       }
+       h1{
+          text-align:  center;
+       }
+  </style>
--- ungdung3/src/firebase.js
+++ ungdung3/src/firebase.js
@@ -10,12 +10,12 @@ const app = initializeApp({
 });
 
 export const db = app.database();
-export const req = db.ref('/reqDatXe')
+export const req = db.ref('reqDatXe/')
 export const DsDaCoXeNhan =  req.orderByChild('tinhTrang').equalTo('da co xe nhan');
 export const DsDaDinhVi =  req.orderByChild('tinhTrang').equalTo('da dinh vi');
 export const DsChuaDinhVi =  req.orderByChild('tinhTrang').equalTo('chua dinh vi');
 export const DsDangDiChuyen =  req.orderByChild('tinhTrang').equalTo('dang di chuyen');
 export const DsDaHoanThanh =  req.orderByChild('tinhTrang').equalTo('da hoan thanh');
 
-export const Driver =  db.ref('/driver')
+export const Driver =  db.ref('driver/')
 
